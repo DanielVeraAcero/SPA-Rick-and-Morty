9bafd56cd5304d17dfc43d192e62f8632d034062
Daniel Vera <d.vera.acero@gmail.com>
2022-10-19 20:28:08 -0500

    add navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../templates/Header.js';
 import Home from '../pages/Home.js';
 import Character from '../pages/Character.js';
 import Error404 from '../pages/Error404.js';
+import getHash from '../utils/getHash.js';
+import resolveRoutes from '../utils/resolveRoutes.js';
 
 const  routes = {
     '/': Home,
@@ -14,6 +16,11 @@ const router = async () => {
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+
+    content.innerHTML = await render();
 }
 
 export default router;
