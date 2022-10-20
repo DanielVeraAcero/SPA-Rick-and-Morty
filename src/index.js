eb660667218d618c2f790e3d899b34b76fd42777
Daniel Vera <d.vera.acero@gmail.com>
2022-10-19 19:07:19 -0500

    add base index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-const getPosts = async () => {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const data = await response.json();
-        console.log(data);        
-    } catch (error) {
-        console.log(error);
-    }
-};
+import router from './routes/index.js'
 
-getPosts();
+window.addEventListener('load', router);
